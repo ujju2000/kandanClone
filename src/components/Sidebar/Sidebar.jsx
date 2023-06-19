44ece8e8867e7d5743bad1a81186044980f0f636
Ujjwal Tarika <ujjutarika.35@gmail.com>
2023-06-19 12:02:12 +0530

    updating thought

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -7,6 +7,14 @@ export default function Sidebar() {
     <div className = 'sidebar'>
         <Option />
         <Projects />
+        <div className="thoughtContainer">
+          <div className="bulbImg">
+            <img src="assets/vector.png" alt="" className='bulb'/>
+          </div>
+          <h6>Thoughts Time</h6>
+          <p>We don’t have any notice for you, till then you can share your thoughts with your peers.</p>
+          <input type="text" name="" id="" placeholder='Write a message' className='thoughtInput'/>
+        </div>
     </div>
 
   )
